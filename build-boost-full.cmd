24586b54cd4bc09b7e9fff8bf78c8e905bcd18e7
Ravil Nugmanov <ravil.nugmanov@gmail.com>
2019-04-26 18:30:41 -0600

    Updated script which builds boost (bootstrap script was used without "call", which was causing skipping of the actual build step)

--- build-boost-full.cmd
+++ build-boost-full.cmd
@@ -3,6 +3,7 @@ REM In case of success, output result will be located at ../boost/stage/x64
 SET SCRIPT_PATH=%~dp0
 for %%i in ("%~dp0.") do SET "SCRIPT_PATH=%%~fi"
 
+REM need specific version of boost, which was used to build the OPC UA SDK binaries.
 SET TAG_BOOST=boost-1.70.0
 
 SET REPO_BASE_FOLDER=%SCRIPT_PATH%\..
@@ -11,7 +12,7 @@ IF EXIST %REPO_BASE_FOLDER%\boost GOTO M_FOLDER_EXITS
 PUSHD %REPO_BASE_FOLDER%
 git clone --recursive --branch %TAG_BOOST% https://github.com/boostorg/boost.git boost
 PUSHD boost
-bootstrap
+call bootstrap
 b2 -j4 toolset=msvc-14.1 variant=release,debug address-model=64 architecture=x86 link=static threading=multi runtime-link=shared --build-type=minimal stage --stagedir=stage/x64
 POPD
 POPD
